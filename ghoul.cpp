e8252185cd9a4d2d0bbd057ac6593900da39f001
Kirsten Corrao <kirstencorrao@gmail.com>
2019-08-11 16:37:22 -0700

    added special actions to ghoul

--- ghoul.cpp
+++ ghoul.cpp
@@ -5,25 +5,28 @@ Author: Kirsten Corrao
 Date: 03/14/2019
 Description: this is the implementation file for GhoulTormenter, which is a derived class of Character.
 ***************************************************************************************************/
-#include "GhoulTormenter.hpp"
+#include "ghoul.hpp"
 
 /********************************* constructor ****************************************************
 Uses the Character base class constructor to create a derived class object. Creates a weapon and 
 armor in the Character's inventory and sets the Character's currentWeapon and currentArmor to them.
 ***************************************************************************************************/
-GhoulTormenter::GhoulTormenter()
-	: Character(6, "Ghoul Tormenter", "")		// HP, type, name
+Ghoul::Ghoul()
+	: Character(6, "Ghoul", "", 'G', 4)		// HP, type, name, symbol, MP
 {
-	inventory.add(new Treasure("Human Arm", 5, 0, 0, Type::weapon));
-	inventory.add(new Treasure("Human Leather", 0, 6, 0, Type::armor));
+	inventory.add(std::make_unique<Treasure>("Human Arm", 5, 0, 0, Type::weapon));
+	inventory.add(std::make_unique<Treasure>("Human Leather", 0, 6, 0, Type::armor));
 
 	currentWeapon = inventory.getTreasure(0);
 	currentArmor = inventory.getTreasure(1);
+
+	specialActions.push_back(std::make_unique<SpecialAction>(ActionType::attack, "Claw Frenzy", "A frenetic assault of half-rotten claws causes 1-10 damage", 2, 1, 10, 0, 0, 0));
+	specialActions.push_back(std::make_unique<SpecialAction>(ActionType::MP, "Blood Elixer", "An elixir made from the blood of human victims restores 2-4 HP", 1, 0, 0, 4, 0));
 }
 
 /********************************* destructor *****************************************************
 New inventory items deleted in Character destructor.
 ***************************************************************************************************/
-GhoulTormenter::~GhoulTormenter()
+Ghoul::~Ghoul()
 {
 }
--- ghoul.hpp
+++ ghoul.hpp
@@ -5,18 +5,18 @@ Author: Kirsten Corrao
 Date: 02/28/2019
 Description: this is the header file for GhoulTormenter, which is a derived class of Character.
 ***************************************************************************************************/
-#ifndef GHOULTORMENTER_HPP
-#define GHOULTORMENTER_HPP
+#ifndef GHOUL_HPP
+#define GHOUL_HPP
 
-#include "Character.hpp"
+#include "character.hpp"
 
-class GhoulTormenter : public Character
+class Ghoul : public Character
 {
 private:
 
 public:
-	GhoulTormenter();
-	~GhoulTormenter();
+	Ghoul();
+	~Ghoul();
 };
 
 #endif
